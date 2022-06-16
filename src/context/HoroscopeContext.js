585e729a7b0d68fb7de90a53be69c3ca2a1bbb93
john0123456789 <johnluongpham@yahoo.com>
2022-06-16 09:12:39 -0700

    added context

---
+++ src/context/HoroscopeContext.js
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+export const HoroscopeContext = createContext();
+
+// export default function HoroscopeProvider(props) {
+
+//   return (
+//     <HoroscopeContext.Provider value={{ sign: "Leo" }}>
+//       {props.children}
+//     </HoroscopeContext.Provider>
+//   )
+// }
